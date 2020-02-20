f4b587809e54d494265890a1543d30b70b962407
Nguyen Lap <nguyenlapdev@gmail.com>
2020-02-20 22:39:12 +0700

    Commit hocgit.php lan 2

--- hocgit.php
+++ hocgit.php
@@ -1,1 +1,9 @@
-File hoc git day
+git status
+git diff
+git commit -a -m "Message"
+
+- Gắn remote từ server xuống localtion
+git remote add tên repository
+
+git remote : xem origin
+git remote rm: để xóa origin
